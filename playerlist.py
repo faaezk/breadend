be8202ecef07ec68ee2878d08a794443172e6c34
faaezk <faaez.kamal@gmail.com>
2021-06-17 11:51:31 +0000

    it stopped connecting again

---
+++ playerlist.py
@@ -0,0 +1,48 @@
+
+players = [("silentwhispers", "0000"), 
+    ("fakinator", "4269"), 
+    ("faqinator", "7895"), 
+    ("8888", "nadi"), 
+    ("dilka30003", "0000"),
+    ("slumonaire", "oce"),
+    ("katchampion", "oce"), 
+    ("imabandwagon", "oce"), 
+    ("giroud", "8383"), 
+    ("oshaoshawott", "oce"), 
+    ("yovivels", "1830"), 
+    ("therealrobdez", "3333"),
+    ("bento2", "box"), 
+    ("hoben222", "9327"), 
+    ("jokii", "oce"),
+    ("lyçhii", "mai"),
+    ("lmao", "6548"),
+    ("jack", "ytb"),
+    ("vkj", "4084"),
+    ("tallewok", "6209"),
+    ("fade", "1280"),
+    ("skzcross", "oce"),
+    ("lol", "4529"),
+    ("crossaxis", "mippl"),
+    ("azatory", "nike"),
+    ("quyteriyaki", "oce")
+    ]
+
+online_players = [ 
+    ["fakinator", "4269"],
+    ["8888", "nadi"], 
+    ["dilka30003", "0000"],
+    ["slumonaire", "oce"],
+    ["hoben222", "9327"],
+    ["silentwhispers", "0000"],
+    ["imabandwagon", "oce"],
+    ["lmao", "6548"]
+    ]
+
+names = {"fakinator" : "Faaez", "8888" : "Hadi", "dilka30003" : "Dhiluka", 
+        "slumonaire" : "Chris", "hfoben222" : "Ben", "silentwhispers" : "Rasindu",
+        "imabandwagon" : "Dylan", "lmao" : "Joseph"}
+
+game_names = {"Faaez" : "fakinator", "Hadi" : "8888", "Dhiluka" : "dilka30003", 
+        "Chris" : "slumonaire", "Ben" : "hoben222", "Rasindu" : "silentwhispers",
+        "Dylan" : "imabandwagon", "Joseph" : "lmao"}
+
--- ritsu.py
+++ ritsu.py
@@ -70,6 +70,12 @@ async def on_message(message):
     if "john" in message.content.lower():
         await message.add_reaction("\u2705")
 
+    if message.content.lower().startswith('=test'):
+        
+        reply = message.content.lower()
+
+        await message.channel.send(reply)    
+
 
 config = get_config()
 token = config[2]
