555e102a47ea8e2ee33fc62d22b0ef4ba6038807
faaezk <faaez.kamal@gmail.com>
2021-06-18 07:23:03 +0000

    python decided it doesn't like relative path's even more, fixed a bunch of graph stuff

--- graphs.py
+++ graphs.py
@@ -7,7 +7,7 @@ players = playerlist.players
 
 def make_graph(username):
 
-    if os.path.isfile('elo_history/{}.txt'.format(username)) == False:
+    if os.path.isfile('/home/ubuntu/discord_bot/elo_history/{}.txt'.format(username)) == False:
         return
     
     tagline = ""
@@ -16,7 +16,7 @@ def make_graph(username):
             tagline = player[1]
     elo_history.update_elo_history(username, tagline)
     
-    file1 = open('elo_history/{}.txt'.format(username), 'r')
+    file1 = open('/home/ubuntu/discord_bot/elo_history/{}.txt'.format(username), 'r')
 
     y = [x.strip() for x in file1.readlines()]
     y.pop(0)
@@ -31,7 +31,7 @@ def make_graph(username):
     plt.ylabel('mmr')
     plt.title(username + '\'s elo but the x axis has no meaning cause i cbs')
 
-    plt.savefig('elo_graphs/{}.png'.format(username))
+    plt.savefig('/home/ubuntu/discord_bot/elo_graphs/{}.png'.format(username))
     file1.close()
     plt.clf()
 
