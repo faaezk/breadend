79f68ec4cb038ed12cfb875fd0eae5b71d3acf88
Faaez Kamal <faaez.kamal@gmail.com>
2021-06-25 01:47:40 +1000

    Wellll, i did stuff... Dhiluka i apologise in advance if i've done something dumb

--- temp.py
+++ temp.py
@@ -1,13 +1,19 @@
 from playerlist import player
+import requests
+import json
 
 class Player():
-    def __init__(self, ign, tag, name = None, onlineList = False, online = False):
+    def __init__(self, ign, tag, name = None, onlineList = False, online = False, status = None, partyid = False, partysize = 0):
         self.ign = ign
         self.tag = tag
         self.name = name
         self.onlineList = onlineList
         self.online = online
-    
+
+        self.status = status
+        self.partyid = partyid
+        self.partysize = partysize
+
     def isOnline(self) -> bool:
         return self.isOnline
     
@@ -19,6 +25,46 @@ class Player():
 
     def __eq__(self, o: object) -> bool:
         return str(self) == str(o)
+
+    def updatestuff(self):
+        url = "https://api.henrikdev.xyz/valorant/v1/live-match/{}/{}".format(self.ign, self.tag)
+        r = requests.get(url, headers={'Cache-Control': 'no-cache'})
+        data = json.loads(r.text)
+
+        igstatus = ""
+
+        if data['status'] == '200':
+            
+            self.partyid = data['data']['party_id']
+
+            if data['data']['current_state'] == 'MENUS':
+                self.partysize = data['data']['party_size']
+
+            state = data['data']['current_state']
+
+            if state == 'PREGAME':
+                igstatus = "Online and in agent select"
+
+            elif state == 'MENUS':
+                igstatus = "Online and in menu"
+
+            elif state == 'INGAME':
+                map = data['data']['map']
+
+                if map == 'Range':
+                    igstatus = "Online in the range"
+
+                else:
+                    game_mode = data['data']['gamemode']
+                    score = str(data['data']['score_ally_team']) + '-' + str(data['data']['score_enemy_team'])
+                    map = data['data']['map']
+                    igstatus = "Online in " + game_mode + " going " + score + " on " + map
+        else:
+            igstatus = "Offline"
+        
+        self.status = igstatus
+
+
 
 class PlayerList():
     def __init__(self, filePath):
