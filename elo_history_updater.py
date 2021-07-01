51ed3ee7e0b6c06481a688bd87075b1a98c7a193
faaezk <faaez.kamal@gmail.com>
2021-07-01 11:35:40 +0000

    elo history updates (also, it kinda worked a lil bit)

--- elo_history_updater.py
+++ elo_history_updater.py
@@ -10,7 +10,7 @@ def update_all_elo_history():
     update_count = 0
     
     for i in range(0, len(playerList.players)):
-        update_count += valorant.update_elo_history(playerList.players[i][0], playerList.players[i][1])
+        update_count += valorant.update_elo_history(playerList.players[i].ign, playerList.players[i].tag)
         #print("completed " + str(i + 1) + "/" + str(len(playerList.players)))
 
     return str(update_count) + " updates"
