bcb45046325ad0a41960466822a68785f21000cb
Faaez Kamal <faaez.kamal@gmail.com>
2021-07-10 19:40:01 +1000

    Seeing if 'on message' can work alongside other commands

--- ritsutoo.py
+++ ritsutoo.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
+import weather
 import configparser
+import valorant_online
+import graphs
 import valorant
 
 def get_config():
@@ -27,5 +30,19 @@ async def elolist(ctx, *, username):
     username = username.lower()
     elolist = valorant.get_elolist(username)
     await ctx.send("```\n" + elolist + "\n```")
+
+@client.command()
+async def weather(ctx):
+    john = weather.main()
+    await ctx.send("Feels like " + str(john['main']['feels_like']) + " degrees today")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower().startswith('good evening'):
+        await message.channel.send(file=discord.File('good_evening.mp4'))
+
 
 client.run(token)
