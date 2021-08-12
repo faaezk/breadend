b91630f9cd5186d9be01336745f7cb6ec250670e
faaezk <faaez.kamal@gmail.com>
2021-08-12 14:05:17 +0000

    added cat command

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import configparser
 import valorant_online
 import graphs
 import valorant
+import requests
+import json
 
 def get_config():
     c = configparser.ConfigParser()
     c.read('/home/ubuntu/discord_bot/config.ini')
 
-    return c['discord']['token']
+    return c['discord']['token'], c['cat']['api']
 
-token = get_config()
+token = get_config()[0]
 
 help_command = commands.DefaultHelpCommand(no_category = 'Commands')
 client = commands.Bot(command_prefix='$',help_command = help_command)
@@ -19,6 +21,24 @@ client = commands.Bot(command_prefix='$',help_command = help_command)
 @client.event
 async def on_ready():
     print("it started working")
+
+@client.command()
+async def cat(ctx):
+    url = "https://api.thecatapi.com/v1/images/search?format=json"
+
+    payload={}
+    files={}
+    headers = {
+    'Content-Type': 'application/json',
+    'x-api-key': get_config()[1]
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload, files=files)
+
+    embed = discord.Embed(title="cat")
+    embed.set_image(url=json.loads(response.text)[0]['url'])
+
+    await ctx.send(embed=embed)
 
 @client.command(
     help="Syntax: $graph username or $graph username#tag", 
