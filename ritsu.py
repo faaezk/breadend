d3d0f187b1e4f4c04d6a1e32251c6019e833fa39
faaezk <faaez.kamal@gmail.com>
2021-09-30 13:03:03 +0000

    elo history updates

--- ritsu.py
+++ ritsu.py
@@ -14,7 +14,7 @@ def get_config():
 token = get_config()
 
 help_command = commands.DefaultHelpCommand(no_category = 'Commands')
-client = commands.Bot(command_prefix='!',help_command = help_command)
+client = commands.Bot(command_prefix='j',help_command = help_command)
 slash = SlashCommand(client, sync_commands=True)
 guild_ids = [731539222141468673]
 
@@ -22,137 +22,42 @@ guild_ids = [731539222141468673]
 async def on_ready():
     print("it started working")
 
+@client.command()
+async def g(ctx, message=None):
 
-@slash.slash(description="search MAL database",
-             guild_ids=guild_ids,
-             options = [create_option(name="anime_title", description="Enter an anime to search for", option_type=3, required=False),
-             create_option(name="manga_title", description="Enter an manga to search for", option_type=3, required=False),
-             create_option(name="character", description="Enter a character to search for", option_type=3, required=False),
-             create_option(name="anime_stats", description="Enter an anime to get stats for", option_type=3, required=False),
-             create_option(name="manga_stats", description="Enter an manga to get stats for", option_type=3, required=False)])
-async def search(ctx, *, anime_title = "", manga_title = "", character = "", anime_stats = "", manga_stats = ""):
-    
-    if anime_title != "":
-        msg = await ctx.send("Getting info for " + anime_title)
-        anime = malsearch.animeSearch(anime_title)
+        member = message.author.name
 
-        if anime == False:
-            await ctx.send("dumb dumb api failed, try again.")
-        
-        elif anime == None:
-            await ctx.send("Anime not found.")
+        webhook = await ctx.channel.create_webhook(name=member.name)
+        await webhook.send(
+            str(message), username=member.name, avatar_url=member.avatar_url)
 
-        else:
-            embed = discord.Embed(title="{} ({})".format(anime['eng_title'], anime['jap_title']), url=anime['url'], 
-            description="Source: {}, Type: {}, Score: {}, Episodes: {}".format(anime['source'], anime['type'], anime['score'], anime["ep_count"]))
-
-            embed.set_image(url=anime['image_url'])
-            embed.add_field(name="Airing Dates:", value=anime["Airing_Dates"])
-            embed.add_field(name="Genres:", value=anime["genres"])
-            embed.add_field(name="Sequel", value=anime["sequel"])
-            embed.add_field(name="Synopsis:", value=anime["synopsis"])
-            embed.add_field(name="Opening Theme", value=anime["opening_themes"], inline=False)
-            embed.add_field(name="Ending Theme", value=anime["ending_themes"], inline=False)
-            embed.set_footer(text="Studios: {}, Licensors: {}".format(anime["studios"], anime["licensors"]))
-            await msg.edit(content="", embed=embed)
-
-    if manga_title != "":
-        msg = await ctx.send("Getting info for " + manga_title)
-        manga = malsearch.mangaSearch(manga_title)
-
-        if manga == False:
-            await ctx.send("dumb dumb api failed, try again.")
-        
-        elif manga == None:
-            await ctx.send("Manga not found.")
-
-        else:
-            embed = discord.Embed(title="{} ({})".format(manga['eng_title'], manga['jap_title']), url=manga['url'], 
-            description="Type: {}, Score: {}, Volumes: {}, Chapters: {}".format(manga['type'], manga['score'], manga["vol_count"], manga["chap_count"]))
-
-            embed.set_image(url=manga['image_url'])
-            embed.add_field(name="Publishing Dates:", value=manga["publishing"])
-            embed.add_field(name="Genres:", value=manga["genres"])
-            embed.add_field(name="Authors:", value=manga["authors"])
-            embed.add_field(name="Synopsis:", value=manga["synopsis"])
-            embed.set_footer(text="Serialisations: {}".format(manga["serialisations"]))
-            await msg.edit(content="", embed=embed)
-
-    if character != "":
-        msg = await ctx.send("Getting info for " + character)
-        character = malsearch.characterSearch(character)
-
-        if character == False:
-            await ctx.send("dumb dumb api failed, try again.")
-        
-        elif character == None:
-            await ctx.send("Character not found.")
-
-        else:
-            embed = discord.Embed(title=character['name'], url=character['url'],
-                                    description="Member favourites: " + str(character['member_favourites']))
-
-            embed.set_image(url=character['image_url'])
-            embed.add_field(name="Description", value=character["description"], inline=False)
-            embed.add_field(name="Anime:", value=character["anime"], inline=False)
-            embed.add_field(name="Manga:", value=character["manga"], inline=False)
-            embed.add_field(name="Voice Actors:", value=character["voice_actors"], inline=False)
-            
-            await msg.edit(content="", embed=embed)
-
-    if anime_stats != "":
-        msg = await ctx.send("Getting stats for " + anime_stats)
-        anime = malsearch.animeStats(anime_stats)
-
-        if anime == False:
-            await ctx.send("dumb dumb api failed, try again.")
-        
-        elif anime == None:
-            await ctx.send("Character not found.")
-
-        else:
-            file=discord.File(fp="/home/ubuntu/discord_bot/image.png", filename='image.png')
-            embed = discord.Embed(title=anime['title'], url=anime['url'])
-
-            embed.set_image(url="attachment://image.png")
-
-            embed.add_field(name="Other stats:", 
-            value="Completed: {}\nWatching: {}\nPlan to watch: {}\nDropped: {}\nOn Hold: {}\nTotal: {}".format(
-                anime["completed"], anime["watching"], anime["plan_to_watch"], anime["dropped"],
-                anime["on_hold"], anime["total"]),
-            inline=False)
-            
-            await msg.edit(content="", file=file, embed=embed)
-
-    if manga_stats != "":
-        msg = await ctx.send("Getting stats for " + manga_stats)
-        manga = malsearch.mangaStats(manga_stats)
-
-        if manga == False:
-            await ctx.send("dumb dumb api failed, try again.")
-        
-        elif manga == None:
-            await ctx.send("Character not found.")
-
-        else:
-            file=discord.File(fp="/home/ubuntu/discord_bot/image.png", filename='image.png')
-            embed = discord.Embed(title=manga['title'], url=manga['url'])
-
-            embed.set_image(url="attachment://image.png")
-
-            embed.add_field(name="Other stats:", 
-            value="Completed: {}\nReading: {}\nPlan to read: {}\nDropped: {}\nOn Hold: {}\nTotal: {}".format(
-                manga["completed"], manga["reading"], manga["plan_to_read"], manga["dropped"],
-                manga["on_hold"], manga["total"]),
-            inline=False)
-            
-            await msg.edit(content="", file=file, embed=embed)
-
+        webhooks = await ctx.channel.webhooks()
+        for webhook in webhooks:
+                await webhook.delete()
 
 @client.event
 async def on_message(message):
+
     if message.author == client.user:
         return
+
+    user = client.get_user(message.author.id)
+
+    print(user)
+    print(message.author.display_name)
+
+    if message.content.startswith('name'):
+        
+        member=message.author
+
+        webhook = await message.channel.create_webhook(name=member.display_name)
+        await webhook.send("changed name\n", username=member.display_name, avatar_url=member.avatar_url)
+
+        webhooks = await message.channel.webhooks()
+        for webhook in webhooks:
+                await webhook.delete()
+
+
 
     if message.content.lower().startswith('good evening'):
         await message.channel.send(file=discord.File('good_evening.mp4'))
