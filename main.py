0ab944e6681b552b43828003d2cb7b52982202e9
faaezk <faaez.kamal@gmail.com>
2021-10-09 00:40:44 +0000

    converted leaderboard to a slash command in main bot

--- main.py
+++ main.py
@@ -160,32 +160,17 @@ async def stats(ctx, *, username):
     else:
         await ctx.send("```\n" + "Player not found, check syntax: (username#tag)" + "\n```")
 
-@client.command(
-    help="Optional argument to update leaderboard with newest data: 'update'\nSyntax: $leaderboard or $leaderboard update", 
-    brief="Returns a MMR leaderboard (updates every 13 minutes)")
-async def leaderboard(ctx, *, command="john"):
+@slash.slash(description="Valorant Leaderboards",
+             guild_ids=guild_ids,
+             options = [
+             create_option(name="options", description="Region/options for leaderboard (leave blank for local)", option_type=3, required=False, 
+                        choices=[create_choice(name="Update local leaderboard",value="update"), create_choice(name="Asia Pacific",value="ap"),
+                        create_choice(name="Europe",value="eu"), create_choice(name="Korea",value="kr"),
+                        create_choice(name="North America",value="na")])])
 
-    if command.lower() == 'update':
-        the_message = await ctx.send("this is gonna take a while...")
-        john = valorant.elo_leaderboard()
-        await the_message.edit(content="```\n" + john + "\n```")
-         
-    elif command.lower() == "na" or command.lower() == "eu" or command.lower() == "ap" or command.lower() == "oce":
-        region = command.lower()
-        if region == "oce":
-            region = "ap"
-        
-        rleaderboard = valorant.region_leaderboard(region)
-
-        if rleaderboard:
-            contents = f'{region.upper()} Ranked Leaderboard\n'
-            contents += rleaderboard
-            await ctx.send("```\n" + valorant.region_leaderboard(region) + "\n```")
-        
-        else:
-            await ctx.send("invalid region. select from: AP, NA, EU, KR")
-
-    else:
+async def leaderboard(ctx, options=""):
+    
+    if options == "":
         john = "this is like, potentially up to 13 minutes old\n"
         f = open("leaderboard.txt", "r")
         for x in f:
@@ -193,6 +178,23 @@ async def leaderboard(ctx, *, command="john"):
         f.close()
 
         await ctx.send("```\n" + john + "\n```")
+    
+    if options == "update":
+        the_message = await ctx.send("this is gonna take a while...")
+        john = valorant.elo_leaderboard()
+        await the_message.edit(content="```\n" + john + "\n```")
+
+    if options == "ap" or options == "eu" or options == "kr" or options == "na":
+
+        regions = {"ap" : "Asia Pacific", "eu" : "Europe", "kr" : "Korea", "na" : "North America"}
+        
+        the_message = await ctx.send("fetching leaderboard...")
+        rleaderboard = valorant.region_leaderboard(options)
+
+        if rleaderboard:
+            contents = f'{regions[options]} Ranked Leaderboard\n'
+            contents += rleaderboard
+            await the_message.edit(content="```\n" + contents + "\n```")
 
 @client.command(
     help="No longer works", 
--- playground.py
+++ playground.py
@@ -1,3 +1,24 @@
-import valorant
+import requests
+import json
 
-print(valorant.region_leaderboard("na"))
+url = f"https://api.henrikdev.xyz/valorant/v1/leaderboard/kr"
+r = requests.get(url)
+
+data = json.loads(r.text)
+players = []
+length = 20
+    
+for i in range(length):
+        players.append([data[i]['gameName'], data[i]['rankedRating']])
+        ign = data[i]['gameName']
+        print(ign + "  " + str(len(ign)))
+
+rleaderboard = ""
+    
+for i in range(0, len(players)):
+
+    elo = players[i][1]
+    user = players[i][0]
+    rank = i + 1
+    rleaderboard += str(str(rank) + '.').ljust(3) + str(user).ljust(20) + str(elo).rjust(5) + '\n'
+
