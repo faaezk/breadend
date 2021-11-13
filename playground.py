7732135ac7a9796923b9f5a81b899f279fb2dd83
faaezk <faaez.kamal@gmail.com>
2021-11-13 04:10:33 +0000

    leaderboard actually fixed now

--- playground.py
+++ playground.py
@@ -8,5 +8,16 @@ melb_now = datetime.now(tz)
 
 
 
-print(now.strftime("%d/%m/%Y") + " at " + now.strftime("%H:%M:%S"))
-print(melb_now.strftime("%d/%m/%Y") + " at " + melb_now.strftime("%H:%M:%S"))
+#print(now.strftime("%d/%m/%Y") + " at " + now.strftime("%H:%M:%S"))
+#print(melb_now.strftime("%d/%m/%Y") + " at " + melb_now.strftime("%H:%M:%S"))
+
+import valorant
+import elo_history_updater
+
+elo_history_updater.update_all_elo_history()
+valorant.elo_leaderboard()
+john = ""
+f = open("leaderboard.txt", "r")
+for x in f:
+    john += x
+f.close()
