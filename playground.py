3f9e13ef58ef94686017eeb3fe82fb7b8a5000f7
Faaez Kamal <faaez.kamal@gmail.com>
2021-11-14 15:22:19 +1100

    name changing stuff

--- playground.py
+++ playground.py
@@ -1,12 +1,6 @@
-from datetime import datetime
-import pytz
+import playerclass
 
-now = datetime.now()
-
-tz = pytz.timezone('Australia/Melbourne')
-melb_now = datetime.now(tz)
-
-
-
-#print(now.strftime("%d/%m/%Y") + " at " + now.strftime("%H:%M:%S"))
-#print(melb_now.strftime("%d/%m/%Y") + " at " + melb_now.strftime("%H:%M:%S"))
+playerList = playerclass.PlayerList('playerlist.csv')
+playerList.load()
+playerList.change_ign('workgffding', 'actuallyworking', 'yay')
+playerList.save()
