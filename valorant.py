16a3633497fb19f5751bc50e78363f5c7a3851db
Faaez Kamal <faaez.kamal@gmail.com>
2021-11-14 20:47:22 +1100

    added a thingy to check server statuses

--- valorant.py
+++ valorant.py
@@ -287,3 +287,40 @@ def check_if_account_exists(ign, tag):
             return False
 
     return john
+
+def servercheck():
+
+    counter = 0
+    report = ""
+    regions = {"ap" : "Asia Pacific", "eu" : "Europe", "kr" : "Korea", "na" : "North America"}
+
+    for elem in regions.keys():
+        
+        url = f'https://api.henrikdev.xyz/valorant/v1/status/{elem}'
+
+        r = requests.get(url)
+
+        if str(r) == "<Response [204]>":
+            break
+
+        john = json.loads(r.text)
+
+        if 'status' in john:
+            if john['status'] != '200':
+                break
+        
+        if 'statusCode' in john:
+            if john['statusCode'] != 200:
+                break
+        
+        maintenances = len(john['data']['maintenances'])
+        incidents = len(john['data']['incidents'])
+        counter += maintenances + incidents
+
+        report += f'{regions[elem]}:\nMaintenances - {maintenances}\nIncidents - {incidents}\n'
+        report += f'{regions[elem]}:\n{maintenances} maintenances and {incidents} incidents\n'
+
+    if counter == 0:
+        return "no maintenances or incidents reported"
+        
+    return report
