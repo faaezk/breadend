4cbb4cd3f8b46e43a373a62ab7f51f7500b6a4e9
Faaez Kamal <faaez.kamal@gmail.com>
2022-04-11 05:19:00 +1000

    elo history updates

--- elo_history_updater.py
+++ elo_history_updater.py
@@ -1,10 +1,10 @@
 import valorant
 from datetime import datetime
-import pytz
+#import pytz
 import playerclass
-#import graphs
+import graphs
 
-def update_all_elo_history():
+def update_all_elo_history(start=0):
     
     playerList = playerclass.PlayerList('playerlist.csv')
     playerList.load()
@@ -12,7 +12,7 @@ def update_all_elo_history():
     update_count = 0
     thing = ""
     
-    for i in range(0, len(playerList.players)):
+    for i in range(start, len(playerList.players)):
 
         if playerList.players[i].active == 'False':
             continue
@@ -21,18 +21,19 @@ def update_all_elo_history():
 
         if thing == "welp":
             return ["welp", i]
+        if type(thing) != str:
+            update_count += int(thing)
+            print('updates: ' + str(update_count))
 
-        update_count += int(thing)
-
-        #print("completed " + str(i + 1) + "/" + str(len(playerList.players)))
-        #graphs.make_graph(playerList.players[i].ign)
+        print("completed " + str(i + 1) + "/" + str(len(playerList.players)))
+        graphs.make_graph(playerList.players[i].ign)
 
     return str(update_count) + " updates"
 
 if __name__ == '__main__':
-    updates = update_all_elo_history()
-    tz = pytz.timezone('Australia/Melbourne')
-    melb_now = datetime.now(tz)
+    updates = update_all_elo_history(0)
+    #tz = pytz.timezone('Australia/Melbourne')
+    melb_now = datetime.now()
 
     if updates[0] == "welp":
         print("welp at " + str(updates[1]) + ": " + melb_now.strftime("%d/%m/%Y") + " at " + melb_now.strftime("%H:%M:%S"))
