0161ab9d217aa681a5161c0a0ea62d1eb05260e0
Faaez Kamal <faaez.kamal@gmail.com>
2022-06-02 23:02:42 +1000

    attempt 2 at removing

--- valorant.py
+++ valorant.py
@@ -421,4 +421,4 @@ def remove_player(ign, tag):
     return f'{ign}#{tag} has been removed'
 
 if __name__ == '__main__':
-    print(update_database('fakinator'))
+    print(get_mmr_history('Luusér', 'OCE'))
