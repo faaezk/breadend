f21a97d420aa6b376e69f0b7a265fb0b877172da
Faaez Kamal <faaez.kamal@gmail.com>
2022-07-29 09:35:53 +1000

    Improvements to dated graphs

--- datedGraphs.py
+++ datedGraphs.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import math
+from adjustText import adjust_text
 
 def minmax_in_range(y:list, index:int, john):
     if john == "max":
@@ -50,29 +51,36 @@ def markgraph(marked, x, yint, ydates, i):
             plt.annotate(ydates[i], 
                         xy=(x[i], yint[i]), 
                         xytext=(x[i] - 4, yint[i] + 30),
-                        arrowprops={'arrowstyle' : '->', 'shrinkA' : 1, 'shrinkB' : 4})
+                        arrowprops={'arrowstyle' : '->', 'shrinkA' : 1, 'shrinkB' : 4},
+                        bbox=dict(boxstyle="round, pad=0.1", fc="yellow"))
         else:
             plt.annotate(ydates[i], 
                         xy=(x[i], yint[i]), 
                         xytext=(x[i] - 4, yint[i] - 30),
-                        arrowprops={'arrowstyle' : '->', 'shrinkA' : 1, 'shrinkB' : 4})
+                        arrowprops={'arrowstyle' : '->', 'shrinkA' : 1, 'shrinkB' : 4},
+                        bbox=dict(boxstyle="round, pad=0.1", fc="yellow"))
+
 
     elif yint[i] > yint[i-1]:
         plt.annotate(ydates[i], 
                     xy=(x[i], yint[i]), 
                     xytext=(x[i] - 4, yint[i] + 30),
-                    arrowprops={'arrowstyle' : '->', 'shrinkA' : 1, 'shrinkB' : 4})
+                    arrowprops={'arrowstyle' : '->', 'shrinkA' : 1, 'shrinkB' : 4},
+                    bbox=dict(boxstyle="round, pad=0.1", fc="yellow"))
+
 
     else:
         plt.annotate(ydates[i], 
                     xy=(x[i], yint[i]), 
                     xytext=(x[i] - 4, yint[i] - 30),
-                    arrowprops={'arrowstyle' : '->', 'shrinkA' : 1, 'shrinkB' : 4})
+                    arrowprops={'arrowstyle' : '->', 'shrinkA' : 1, 'shrinkB' : 4},
+                    bbox=dict(boxstyle="round, pad=0.1", fc="yellow"))
+
 
     marked.append(i)
     return marked
 
-def cgraph():
+def agraph():
     y = ["1724,Sunday-July-10-2022-7:42-AM", "1707,Sunday-July-10-2022-8:18-AM", "1700,Sunday-July-10-2022-9:14-AM", "1721,Sunday-July-10-2022-9:52-AM", 
         "1710,Sunday-July-10-2022-12:22-PM", "1723,Sunday-July-10-2022-1:07-PM", "1712,Sunday-July-10-2022-1:55-PM", "1700,Monday-July-11-2022-12:28-PM",
         "1720,Monday-July-11-2022-1:19-PM",  "1736,Monday-July-11-2022-2:08-PM", "1750,Monday-July-11-2022-2:54-PM", "1736,Monday-July-11-2022-4:52-PM", 
@@ -196,4 +204,158 @@ def cgraph():
     plt.title('MMR over tie')
     plt.savefig('date-graph.png', bbox_inches="tight")
 
-cgraph()
+
+texts = []
+
+def mark_grapha(marked, x, yint, ydates, i):
+
+    global texts
+    texts.append(plt.text(s=ydates[i], x=x[i], y=yint[i]))
+
+    # , bbox=dict(boxstyle="square, pad=0.25", fc="yellow"), size='small')
+
+    marked.append(i)
+    return marked
+
+def bgraph():
+    y = ["1724,Sunday-July-10-2022-7:42-AM", "1707,Sunday-July-10-2022-8:18-AM", "1700,Sunday-July-10-2022-9:14-AM", "1721,Sunday-July-10-2022-9:52-AM", 
+        "1710,Sunday-July-10-2022-12:22-PM", "1723,Sunday-July-10-2022-1:07-PM", "1712,Sunday-July-10-2022-1:55-PM", "1700,Monday-July-11-2022-12:28-PM",
+        "1720,Monday-July-11-2022-1:19-PM",  "1736,Monday-July-11-2022-2:08-PM", "1750,Monday-July-11-2022-2:54-PM", "1736,Monday-July-11-2022-4:52-PM", 
+        "1755,Tuesday-July-12-2022-9:51-AM", "1741,Tuesday-July-12-2022-2:05-PM", "1755,Tuesday-July-12-2022-2:54-PM","1771,Tuesday-July-12-2022-3:59-PM",
+        "1768,Tuesday-July-12-2022-4:44-PM", "1755,Tuesday-July-12-2022-4:48-PM", "1772,Wednesday-July-13-2022-7:41-AM", "1757,Wednesday-July-13-2022-8:28-AM", 
+        "1774,Wednesday-July-13-2022-9:20-AM", "1796,Wednesday-July-13-2022-12:08-PM" ,"1816,Wednesday-July-13-2022-12:41-PM", "1801,Wednesday-July-13-2022-1:18-PM", 
+        "1800,Wednesday-July-13-2022-4:35-PM", "1785,Thursday-July-14-2022-12:11-PM", "1810,Thursday-July-14-2022-12:58-PM", "1824,Thursday-July-14-2022-3:17-PM", 
+        "1840,Thursday-July-14-2022-4:00-PM", "1861,Friday-July-15-2022-5:33-AM", "1843,Friday-July-15-2022-6:06-AM", "1862,Friday-July-15-2022-6:40-AM", 
+        "1880,Friday-July-15-2022-10:00-AM", "1864,Friday-July-15-2022-12:08-PM", "1848,Friday-July-15-2022-12:50-PM", "1868,Saturday-July-16-2022-1:58-PM", 
+        "1852,Saturday-July-16-2022-2:39-PM", "1840,Saturday-July-16-2022-3:38-PM", "1827,Saturday-July-16-2022-4:27-PM", 
+        "1847,Sunday-July-17-2022-6:52-AM",  "1865,Sunday-July-17-2022-12:17-PM", "1847,Sunday-July-17-2022-1:14-PM", "1831,Sunday-July-17-2022-1:50-PM", 
+        "1846,Sunday-July-17-2022-3:28-PM", "1832,Sunday-July-17-2022-4:07-PM", "1852,Monday-July-18-2022-1:05-PM", "1834,Tuesday-July-19-2022-4:53-AM", 
+        "1854,Tuesday-July-19-2022-5:39-AM", "1837,Tuesday-July-19-2022-12:14-PM", "1823,Tuesday-July-19-2022-1:08-PM", "1808,Tuesday-July-19-2022-3:50-PM", 
+        "1825,Tuesday-July-19-2022-4:23-PM", "1846,Wednesday-July-20-2022-6:14-AM", "1831,Wednesday-July-20-2022-6:52-AM", "1814,Wednesday-July-20-2022-7:38-AM", 
+        "1800,Wednesday-July-20-2022-8:19-AM"]
+
+    x = list(range(1, len(y) + 1))
+    total = len(y)
+    num = 0
+
+    if num > 1 and (num - 1) < len(y):
+        y = y[-num:]
+    else:
+        num = 0
+        total = 0
+
+    yint = []
+    ydates = []
+    years = []
+    for i in range(0, len(y)):
+        yint.append(int(y[i].split(',')[0]))
+        ts = y[i].split(',')[1].split('-')
+        ydates.append(f'{ts[2]}/{months[ts[1]]}')
+        
+        added = False
+        for year in years:
+            if ts[3][2:] == year[0]:
+                added = True
+                if i > year[1]:
+                    year[1] = i
+            
+        if not added:
+            years.append([ts[3][2:], i])
+
+    ymin = rounddown(min(yint) - 1) + 50
+    ymax = roundup(max(yint) + 1) + 25
+
+    axes = plt.gca()
+    axes.set_ylim([ymin,ymax])
+
+    ticks = []
+    i = int(math.floor((ymin) / 50.0)) * 50
+
+    ranger = int((ymax-ymin)/100)
+
+    while i <= int(math.ceil(ymax / 25.0)) * 25:
+        ticks.append(i)
+        if ranger == 1:
+            i += 20
+        elif ranger < 4:
+            i += 25
+        elif ranger < 8:
+            i += 50
+        else:
+            i += 100
+
+    axes.set_yticks(ticks)
+    labely = []
+
+    for value in ticks:
+        if value % 100 != 0:
+            labely.append(str(value))
+
+        else:
+            labely.append(ranks[value])
+
+    tickx = []  
+    i = 0
+    j = 0
+
+    if len(x) <= 15:
+        j = 1
+    elif len(x) < 30:
+        j = 2
+    elif len(x) < 70:
+        j = 5
+    elif len(x) < 150:
+        j = 10
+
+    if len(x) < 150:
+        while i <= len(x):
+            tickx.append((total - num) + i)
+            i += j
+
+        if x[-1] not in tickx:
+            if (x[-1] - tickx[-1]) < 4:
+                tickx[-1] = x[-1]
+            else:
+                tickx.append(x[-1])
+
+        axes.set_xticks(tickx)
+
+    axes.set_yticklabels(labely)
+    p = plt.plot(x, yint, 'bx-')
+    marked = []
+
+    peakMMRIndex = get_index(yint, max(yint))
+    bottomMMRIndex = get_index(yint, min(yint))
+    marked = mark_grapha(marked, x, yint, ydates, peakMMRIndex)
+    marked = mark_grapha(marked, x, yint, ydates, bottomMMRIndex)
+    marked = mark_grapha(marked, x, yint, ydates, 0)
+    marked = mark_grapha(marked, x, yint, ydates, -1)
+
+    i = 0
+    while i < len(yint):
+        mark = True
+        for j in range((i - 3), (i + 3)):
+            if j in marked:
+                mark = False
+                break
+            
+        if mark:
+            marked = mark_grapha(marked, x, yint, ydates, i)
+        
+        i += 10
+
+    for year in years:
+        plt.vlines(year[1], ymin, ymax, linestyles ="dotted", colors ="k", label=f'20{year[0]}')
+
+    global texts
+    adjust_text(texts, arrowprops=dict(arrowstyle='->'), force_points=30)
+
+    colour = p[0].get_color()
+    plt.axhline(y=y[-1], color=colour, linestyle='dotted')
+    plt.xlabel('Games played')
+    plt.ylabel('MMR')
+    plt.title('MMR over tie')
+    plt.legend(loc='upper left')
+    plt.savefig('date-graph.png', bbox_inches="tight")
+
+bgraph()
