9b27d20775edc90b4f16b74cec2c109ba9472026
Faaez Kamal <faaez.kamal@gmail.com>
2022-08-09 20:09:56 +1000

    I think the api is being weird, maybe

---
+++ mmr_history_updater.py
@@ -0,0 +1,61 @@
+import beta_valorant
+from datetime import datetime
+import beta_playerclass
+import beta_graphs
+
+def update_all(graph, printer=True,start=0):
+    
+    playerList = beta_playerclass.PlayerList('playerlistb.csv')
+    playerList.load()
+    playerList.sort()
+
+    update_count = 0
+    error_count = 0
+    updatedList = []
+    total = str(len(playerList.players) - start)
+
+    for i in range(start, len(playerList.players)):
+        player = playerList.players[i]
+
+        if player.active == 'False':
+            continue
+
+        thing = beta_valorant.update_database(player.puuid)
+
+        if not thing[0]:
+            error_count += 1
+
+            if printer:
+                print(f'{thing[1]} at {player.ign}')
+
+        else:
+            update_count += int(thing[1])
+
+            if int(thing[1]) > 0:
+                updatedList.append((player.ign, thing[1]))
+                
+            if graph:
+                beta_graphs.make_graph(puuid=player.puuid, ign=player.ign, update=False)
+        
+        if printer:
+            print(f'completed {i+1}/{total}')
+
+    if printer:
+        print(updatedList)
+
+    if error_count == 0:
+        updates = f'{update_count} updates'
+    else:
+        updates = f'{update_count} updates, {error_count} errors'
+
+    melb_now = datetime.now()
+    
+    printerz = f'completed on: {melb_now.strftime("%d/%m/%y")} at {melb_now.strftime("%H:%M:%S")} with {updates}'
+    f = open("updater_log-2022.out", "a")
+    f.write(printerz + '\n')
+    f.close()
+
+    return printerz
+
+if __name__ == "__main__":
+    update_all(True)
