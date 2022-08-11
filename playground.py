8a44e240636161115b6857e228881eb9543e4868
Faaez Kamal <faaez.kamal@gmail.com>
2022-08-12 00:02:43 +1000

    became a little less stupid

--- playground.py
+++ playground.py
@@ -1,26 +1,5 @@
 
-import beta_playerclass
-import os
+f = '444r'
 
-playerList = beta_playerclass.PlayerList('playerlistb.csv')
-playerList.load()
-
-folder = 'mmr_history'
-fileList = []
-
-for player in playerList.players:
-    fileList.append(f'{player.ign}.txt')
-
-for file in os.listdir(folder):
-
-    if file in fileList:
-        old_name = os.path.join(folder, file)
-        ign = os.path.splitext(file)[0]
-
-        puuid = playerList.get_puuid_by_ign(ign)
-        new_base = f"{puuid}.txt"
-        new_name = os.path.join(folder, new_base)
-        os.rename(old_name, new_name)
-
-res = os.listdir(folder)
-print(res)
+if f:
+    print(f)
