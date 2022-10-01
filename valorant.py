9cecfe4656415f0edd4314309ef3ee6216d20290
Faaez Kamal <faaez.kamal@gmail.com>
2022-10-02 09:41:51 +1000

    mmr updates and clean up

--- valorant.py
+++ valorant.py
@@ -1,3 +1,4 @@
+import math
 import os
 import requests
 import json
@@ -7,15 +8,20 @@ from io import BytesIO
 import random
 import configparser
 
-def get_data(category, ign="", tag="", region="", crosshair_code=""):
+def get_key():
+    c = configparser.ConfigParser()
+    c.read('config.ini')
 
+    return c['henrik']['key']
+
+def get_data(category, puuid="None", ign="", tag="", region="", crosshair_code=""):
     key = get_key()
     headers = {'accept' : 'application/json', 'Authorization' : key}
     errors = {1 : "Invalid API Key", 2 : "Forbidden endpoint", 3 : "Restricted endpoint", 101 : "No region found for this Player",
             102 : "No matches found, can't get puuid", 103 : "Possible name change detected, can't get puuid. Please play one match, wait 1-2 minutes and try it again",
             104 : "Invalid region", 105 : "Invalid filter", 106 : "Invalid gamemode", 107 : "Invalid map", 108 : "Invalid locale",
             109 : "Missing name", 110 : "Missing tag", 111 : "Player not found in leaderboard", 112 : "Invalid raw type",
-            113 : "Invalid match or player id", 114 : "Invalid country code", 115 : "Invalid season", 408 : "Request timeout", 429 : 'welp', 
+            113 : "Invalid match or player id", 114 : "Invalid country code", 115 : "Invalid season", 429 : 'welp', 
             403 : 'Forbidden', 404 : 'User not found', 500 : 'No matches available'}
 
     if region != "":
@@ -30,22 +36,33 @@ def get_data(category, ign="", tag="", region="", crosshair_code=""):
         return requests.get(url, headers=headers)
 
     else:
-        if ign == "":
-            return(False, False)
+        if puuid != "None":
+            if category == "account":
+                url = f'https://api.henrikdev.xyz/valorant/v1/by-puuid/account/{puuid}'
 
-        if tag == "":
-            tag = get_tag(ign)
-            if not tag:
-                return (False, 'tag not found')
+            elif category == "mmr":
+                url = f'https://api.henrikdev.xyz/valorant/v2/by-puuid/mmr/ap/{puuid}'
 
-        if category == "account":
-            url = f'https://api.henrikdev.xyz/valorant/v1/account/{ign}/{tag}'
+            elif category == "mmr history":
+                url = f'https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr-history/ap/{puuid}'
 
-        elif category == "mmr":
-            url = f'https://api.henrikdev.xyz/valorant/v2/mmr/ap/{ign}/{tag}'
-        
-        elif category == "mmr history":
-            url = f"https://api.henrikdev.xyz/valorant/v1/mmr-history/ap/{ign}/{tag}"
+        else:
+            if ign == "":
+                return (False, 'no ign or puuid given/player not in database')
+
+            if tag == "":
+                tag = get_tag(ign)
+                if not tag:
+                    return (False, 'tag not found')
+
+            if category == "account":
+                url = f'https://api.henrikdev.xyz/valorant/v1/account/{ign}/{tag}'
+
+            elif category == "mmr":
+                url = f'https://api.henrikdev.xyz/valorant/v2/mmr/ap/{ign}/{tag}'
+            
+            elif category == "mmr history":
+                url = f"https://api.henrikdev.xyz/valorant/v1/mmr-history/ap/{ign}/{tag}"
     
     r = requests.get(url, headers=headers)
 
@@ -55,246 +72,212 @@ def get_data(category, ign="", tag="", region="", crosshair_code=""):
     if r.status_code == 200:
         john = json.loads(r.text)
 
-        if ign != "":
+        if category == 'leaderboard':
+            return (True, john)
 
-            if category == 'mmr history':
-                if john['name'] == None or john['tag'] == None or ('error' in john.keys()):
-                    return (False, 'name/tag error?')
+        if 'error' in john.keys() and john['error'] != None:
+            return (False, john['error']['message'])
+        
+        if 'errors' in john.keys():
+            errs = ""
+            for error in john['errors']:
+                errs += f'{error}, '
             
-            elif john['data']['name'] == None or john['data']['tag'] == None or ('error' in john.keys()):
+            if errs != "":
+                return (False, errs[:-2])
+
+        if category == 'mmr history':
+            if john['name'] == None or john['tag'] == None:
+                return (False, 'name/tag error?')
+        
+        elif category != 'status':
+            if john['data']['name'] == None or john['data']['tag'] == None:
                 return (False, 'name/tag error?')
         
         return (True, john)
 
     return (False, 'some error')
 
-def get_key():
-    c = configparser.ConfigParser()
-    c.read('config.ini')
-
-    return c['henrik']['key']
-
 def get_tag(ign):
-    playerlist = playerclass.PlayerList("playerlist.csv")
+    playerlist = playerclass.PlayerList("playerlistb.csv")
     playerlist.load()
 
     for player in playerlist.players:
         if ign == player.ign:
             return player.tag
     
-    return False
+    return "False"
 
-def get_file_mmr(ign):
+def get_file_mmr(puuid):
 
-    file_path = f'elo_history/{ign}.txt'
-
-    if os.path.isfile(file_path) == False:
+    if os.path.isfile(f'mmr_history/{puuid}.txt') == False:
         return False
+    
+    with open(f'mmr_history/{puuid}.txt', 'r') as f:
+        for line in f:
+            pass
 
-    f = open(file_path, 'r')
-    lines = f.readlines()
-    f.close()
-
-    if lines[-1] == '\n':
+    if line == '\n':
         return False
 
     #return the latest MMR value in file
-    return int(lines[-1].split(',')[0])
+    return int(line.split(',')[0])
 
-def initialise_file(ign):
+def initialise_file(puuid):
 
-    f = open(f'elo_history/{ign}.txt', "x")
+    f = open(f'mmr_history/{puuid}.txt', "x")
     f.close()
 
-    f = open(f'elo_history/{ign}.txt', "w")
-    f.writelines('\n')
+    f = open(f'mmr_history/{puuid}.txt', "w")
+    f.write('\n')
     f.close()
 
-def update_database(ign, tag=""):
+def replace_all(string: str, oldValues, newValue):
+    for value in oldValues:
+        string = string.replace(value, newValue)
+    
+    return string
 
-    player_data = get_data('mmr history', ign=ign, tag=tag)
-    if not player_data[0]:
-        return player_data
-    else:
-        player_data = player_data[1]
+def update_database(puuid):
 
-    player_file_path = f'elo_history/{ign}.txt'
+    data = get_data('mmr history', puuid=puuid)
+    if not data[0]:
+        return data
 
-    if os.path.isfile(player_file_path) == False:
-        if len(player_data['data']) == 0:
+    data = data[1]['data']
+
+    if os.path.isfile(f'mmr_history/{puuid}.txt') == False:
+        if len(data) == 0:
             return (False, 'not enough data')
         else:
-            initialise_file(ign)
+            initialise_file(puuid)
     
     # Dates of last update
-    date_raw = player_data["data"][0]["date_raw"]
+    date_raw = data[0]['date_raw']
+    lines = []
 
-    player_file = open(player_file_path, 'r')
-    first_line = player_file.readline()
+    with open(f'mmr_history/{puuid}.txt', 'r') as f:
+        for line in f:
+            lines.append(line)
     
-    if first_line == '\n':
-        last_file_update = 0
-    else:
-        last_file_update = int(first_line)
-    
-    player_file.close()
-
+    last_file_update = 0 if lines[0] == '\n' else int(lines[0])
     new_list = []
 
     if last_file_update == 0:
-
-        for i in range(len(player_data['data'])):
-            thedate = player_data['data'][i]['date'].replace(',', '-').replace(' ', '-').replace('--', '-')
-            new_list.append(str(player_data['data'][i]['elo']) + ',' + thedate)
-
+        for i in range(len(data)):
+            thedate = replace_all(data[i]['date'], [', ', ' '], '-')
+            new_list.append(f"{data[i]['elo']},{thedate}\n")
 
     elif len(str(last_file_update)) == 13:
-        
         if (len(str(date_raw)) == 10):
-            last_file_update = int(str(last_file_update)[:-3])
-            last_num = int(str(last_file_update)[-1])
+            last_file_update = math.floor(last_file_update/1000)
+            last_num = last_file_update // 10**0 % 10
             last_num += 1
-            last_file_update = int(str(last_file_update)[:-1] + str(last_num))
+            last_file_update = math.floor(last_file_update/10) + last_num
 
-        for i in range(len(player_data['data'])):
-
-            date_raw = player_data['data'][i]['date_raw']
-
+        for i in range(len(data)):
+            date_raw = data[i]['date_raw']
             if last_file_update < date_raw:
-                thedate = player_data['data'][i]['date'].replace(',', '-').replace(' ', '-').replace('--', '-')
-                new_list.append(str(player_data['data'][i]['elo']) + ',' + thedate)
-
+                thedate = replace_all(data[i]['date'], [', ', ' '], '-')
+                new_list.append(f"{data[i]['elo']},{thedate}\n")
             else:
                 break
     
     else:
-        for i in range(len(player_data['data'])):
-
-            date_raw = player_data['data'][i]['date_raw']
-
+        for i in range(len(data)):
+            date_raw = data[i]['date_raw']
             if last_file_update < date_raw:
-                thedate = player_data['data'][i]['date'].replace(',', '-').replace(' ', '-').replace('--', '-')
-                new_list.append(str(player_data['data'][i]['elo']) + ',' + thedate)
-
+                thedate = replace_all(data[i]['date'], [', ', ' '], '-')
+                new_list.append(f"{data[i]['elo']},{thedate}\n")
             else:
                 break
     
     if new_list != []:
-        new_list = new_list[::-1]
+        lines[0] = f"{data[0]['date_raw']}\n"
+        lines += new_list[::-1]
 
-        #update timestamp in file
-        with open(player_file_path) as f:
-            lines = f.readlines()
-        
-        lines[0] = str(player_data["data"][0]["date_raw"]) + '\n'
-
-        with open(player_file_path, "w") as f:
+        with open(f'mmr_history/{puuid}.txt', "w") as f:
             f.writelines(lines)
-
-        player_file = open(player_file_path, 'a')
-        
-        for elem in range(len(new_list)):
-            player_file.writelines(str(new_list[elem]) + '\n')
-
-        player_file.close()
     
     return (True, len(new_list))
 
-def get_elo_list(ign):
+def get_elo_list(puuid):
     
-    tag = get_tag(ign)
-    if not tag:
-        return "Player not found"
+    check = update_database(puuid)
+    if not check[0]:
+        return check
 
-    update_database(ign, tag)
-
-    if not get_file_mmr(ign):
-        return "Player not found"
+    if not get_file_mmr(puuid):
+        return (False, "Player not found")
     
-    file1 = open(f'elo_history/{ign}.txt', 'r')
-
-    lines = [x.strip() for x in file1.readlines()]
+    lines = []
+    with open(f'mmr_history/{puuid}.txt', 'r') as f:
+        for line in f:
+            lines.append(line.strip())
 
     if len(lines) == 1:
-        return "No comp games recorded"
+        return (False, "No comp games recorded")
         
     lines.pop(0)
-    
     elolist = ""
-
     for elem in lines:
-        elolist += elem.split(',')[0] + ", "
+        elolist += f"{elem.split(',')[0]}, "
 
-    return elolist[:-2]
+    return (True, elolist[:-2])
 
-def local_leaderboard():
+def leaderboard(region, length=20):
 
-    playerlist = playerclass.PlayerList('playerlist.csv')
-    playerlist.load()
+    if region == 'local':
+        playerlist = playerclass.PlayerList('playerlistb.csv')
+        playerlist.load()
+        players = []
 
-    players = []
+        for player in playerlist.players:
+            mmr = get_file_mmr(player.puuid)
+            if mmr:
+                players.append((player.ign, mmr))
+            
+        players.sort(key=lambda x:x[1], reverse=True)
+        leaderboard = "Player Leaderboard\n"
+    
+    else:
+        data = get_data('leaderboard', region=region)
+        if not data[0]:
+            return data[1]
+        else:
+            data = data[1]
 
-    for player in playerlist.players:
-
-        mmr = get_file_mmr(player.ign)
-
-        if type(mmr) == int:
-            players.append((player.ign, mmr))
+        regions = {"ap" : "Asia Pacific", "eu" : "Europe", "kr" : "Korea", "na" : "North America"}
+        players = []
         
-    players.sort(key=lambda x:x[1], reverse=True)
+        for i in range(length):
+            if data[i]['IsAnonymized'] == True:
+                players.append(("Anonymous", data[i]['rankedRating']))
+            else:
+                players.append((data[i]['gameName'], data[i]['rankedRating']))
 
-    leaderboard = "Player Leaderboard\n"
+        leaderboard = f'{regions[region]} Ranked Leaderboard\n'
 
     for i in range(len(players)):
-
-        ign = players[i][0]
-        mmr = players[i][1]
         rank = i + 1
-        leaderboard += (str(rank) + '.').ljust(3) + str(ign).ljust(16) + str(mmr).rjust(5) + '\n'
+        leaderboard += (str(rank) + '.').ljust(3) + str(players[i][0]).ljust(16) + str(players[i][1]).rjust(5) + '\n'
 
-    f = open("leaderboard.txt", "w")
-    f.write(leaderboard)
-    f.close()
+    if region == 'local':
+        with open("leaderboard.txt", "w") as f:
+            f.write(leaderboard)
 
     return leaderboard
 
-def region_leaderboard(region, length=20):
+def stats(ign="", tag="", puuid="None"):
 
-    data = get_data('leaderboard', region=region)
-    if not data[0]:
-        return data[1]
-    else:
-        data = data[1]
+    if ign == "" and puuid == "None":
+        return (False, False)
 
-    regions = {"ap" : "Asia Pacific", "eu" : "Europe", "kr" : "Korea", "na" : "North America"}
-    players = []
-    
-    for i in range(length):
-        if data[i]['IsAnonymized'] == True:
-            players.append(("Anonymous", data[i]['rankedRating']))
-        else:
-            players.append((data[i]['gameName'], data[i]['rankedRating']))
-
-
-    leaderboard = f'{regions[region]} Ranked Leaderboard\n'
-    
-    for i in range(len(players)):
-
-        ign = players[i][0]
-        mmr = players[i][1]
-        rank = i + 1
-        leaderboard += (str(rank) + '.').ljust(3) + str(ign).ljust(16) + str(mmr).rjust(5) + '\n'
-
-    return leaderboard    
-
-def stats(ign, tag=""):
-
-    john = get_data('mmr', ign=ign, tag=tag)
+    john = get_data('mmr', puuid=puuid, ign=ign, tag=tag)
     if not john[0]:
-        return john[1]
+        return john
     else:
         john = john[1]
-    
     
     data = john['data']['by_season']
     keys = data.keys()
@@ -309,30 +292,30 @@ def stats(ign, tag=""):
             rank = data[key]['final_rank_patched']
             final.append([f'Episode {key[1]} Act {key[3]}:', f'{rank}\nGames Played: {games}\nWinrate: {round((wins/games) * 100, 2)}%'])
     
-    john = get_data('account', ign=ign, tag=tag)
+    john = get_data('account', puuid=puuid, ign=ign, tag=tag)
     if not john[0]:
-        return john[1]
+        return john
     else:
         john = john[1]
 
     card = john['data']['card']['small']
+    return (True, [final, card])
 
-    return [final, card]
+def get_banner(ign="", tag="", puuid="None"):
 
-def get_banner(ign, tag):
+    if ign == "" and puuid == "None":
+        return (False, 'no ign or puuid given')
 
-    data = get_data('account', ign=ign, tag=tag)
+    data = get_data('account', puuid=puuid, ign=ign, tag=tag)
     if not data[0]:
-        return data[1]
+        return data
     else:
         data = data[1]
     
     url = data['data']['card']['large']
     r = requests.get(url, allow_redirects=True)
-
     open('banner.png', 'wb').write(r.content)
-
-    return True
+    return (True, True)
 
 def servercheck():
     counter = 0
@@ -361,14 +344,17 @@ def servercheck():
 
 def add_player(ign, tag):
 
-    playerlist = playerclass.PlayerList("playerlist.csv")
+    playerlist = playerclass.PlayerList("playerlistb.csv")
     playerlist.load()
 
-    if not get_data('account', ign=ign, tag=tag)[0]:
+    data = get_data('account', ign=ign, tag=tag)
+    if not data[0]:
         return "Account does not exist"
+    else:
+        data = data[1]
 
-    player = playerclass.Player(ign.lower(), tag.lower())
-
+    puuid = data['data']['puuid']
+    player = playerclass.Player(ign.lower(), tag.lower(), puuid)
     if playerlist.inList(player):
         return "Account already added"
     
@@ -377,14 +363,15 @@ def add_player(ign, tag):
 
     return f'{ign}#{tag} successfully added'
 
-def remove_player(ign, tag):
+def remove_player(ign):
 
-    playerlist = playerclass.PlayerList("playerlist.csv")
+    playerlist = playerclass.PlayerList("playerlistb.csv")
     playerlist.load()
+    tag = get_tag(ign)
+    puuid = playerlist.get_puuid_by_ign(ign)
+    player = playerclass.Player(ign.lower(), tag, puuid)
 
-    player = playerclass.Player(ign.lower(), tag.lower())
-
-    if not playerlist.inList(player):
+    if puuid == "None" or not tag or not playerlist.inList(player):
         return "Player not in list"
         
     playerlist.remove(player)
@@ -411,11 +398,12 @@ def random_crosshair():
                     "Fishnet"     : "0;P;o;1;d;1;a;0;f;0;0t;8;0l;2;0o;5;0a;0;0f;0;1l;8;1o;2;1a;0;1m;0;1f;0"}
 
     name, code = random.choice(list(crosshairs.items()))
-
     if not crosshair(code):
         return (False, False)
 
     return (name, code)
 
 if __name__ == '__main__':
-    print(update_database('oshawott'))
+    playerlist = playerclass.PlayerList('playerlistb.csv')
+    playerlist.load()
+    print(update_database(playerlist.get_puuid_by_ign('oshawott')))
