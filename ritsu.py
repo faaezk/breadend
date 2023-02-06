ec28369854e87e3f82a7ad5f952b5aedb3e38466
Faaez Kamal <faaez.kamal@gmail.com>
2023-02-06 22:57:09 +1100

    tbh, idk what happened

--- ritsu.py
+++ ritsu.py
@@ -1,6 +1,16 @@
+import discord
 from discord.ext import commands
 import configparser
-import discord
+import graphs
+import valorant
+import mmr_history_updater
+import requests
+import json
+import malsearch
+from discord_slash import SlashCommand
+from discord_slash.utils.manage_commands import create_option, create_choice
+import random
+import playerclass
 from discord import app_commands
 
 def get_config():
@@ -9,22 +19,45 @@ def get_config():
     return c['discord']['ritsu']
 
 token = get_config()
-intents = discord.Intents.default() 
-client = discord.Client(intents=intents)
-tree = app_commands.CommandTree(client)
-guild_id = 731539222141468673
+intents = discord.Intents.default()
+intents.members = True
+help_command = commands.DefaultHelpCommand(no_category = 'Commands')
+client = commands.Bot(command_prefix='$',help_command = help_command, intents=intents)
+slash = SlashCommand(client, sync_commands=True)
+guild_ids = [509314650265878530, 731539222141468673]
 
-# sync the slash command to your server
 @client.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=guild_id))
-    # print "ready" in the console when the bot is ready to work
-    print("ready")
+    print("it started working")
+    try:
+        synced = await client.tree.sync()
+        print(f'Synced {len(synced)} commands')
+    except Exception as e:
+        print(e)
 
-# make the slash command
-@tree.command(name="name", description="description", guild=discord.Object(id=guild_id))
-async def slashing_commanding(int: discord.Interaction, input: str):    
-    await int.response.send_message("command" + input)
+@client.tree.command(name="hello")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"yo {interaction.user.mention}", ephemeral=True)
 
-# run the bot
+
+@client.tree.command(name="rps")
+@app_commands.choices(choices=[
+    app_commands.Choice(name="Rock", value="rock"),
+    app_commands.Choice(name="Paper", value="paper"),
+    app_commands.Choice(name="Scissors", value="scissors"),
+    ])
+async def rps(interaction: discord.Interaction, choices: app_commands.Choice[str]):
+    if (choices.value == 'rock'):
+        counter = 'paper'
+    elif (choices.value == 'paper'):
+        counter = 'scissors'
+    else:
+        counter = 'rock'
+
+    await interaction.response.send_message(f"yo {counter}")
+    
+@client.event
+async def on_message(message):
+    await client.process_commands(message)
+
 client.run(token)
