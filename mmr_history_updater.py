2886202a4c31a42bb99c31b0d92828a00cd1e209
Faaez Kamal <faaez.kamal@gmail.com>
2023-03-10 22:18:26 +1100

    updated updater and graphs

--- mmr_history_updater.py
+++ mmr_history_updater.py
@@ -15,9 +15,7 @@ def update_all(graph, printer=True,start=0):
     retry = []
     total = str(len(playerList.players) - start)
 
-    for i in range(start, len(playerList.players)):
-        player = playerList.players[i]
-
+    for i, player in enumerate(playerList.players):
         if player.active == 'False':
             continue
 
@@ -25,53 +23,46 @@ def update_all(graph, printer=True,start=0):
 
         if not thing[0]:
 
-            if player.priority == '1':
-                retry.append(player)
-            
+            retry.append(player)
             error_count += 1
 
             if printer:
-                print(f'{thing[1]} at {player.ign}')
+                print(f'{i+1}/{total}: {thing[1]} at {player.ign}')
 
         else:
-            update_count += int(thing[1])
+            new_games = int(thing[1])
+            update_count += new_games
 
-            if int(thing[1]) > 0:
-                updatedList.append((player.ign, thing[1]))
+            if new_games > 0:
+                updatedList.append((player.ign, new_games))
                 
             if graph:
-                graphs.make_graph(puuid=player.puuid, ign=player.ign, update=False)
+                graphs.graph(puuid=player.puuid, ign=player.ign, update=False)
         
-        if printer:
-            print(f'completed {i+1}/{total}')
-    
-    i = 0
+            if printer:
+                print(f'{i+1}/{total}: success')
+
     total = len(retry)
-    for player in retry:
-        i += 1
-        if player.active == 'False':
-            continue
-
+    for i, player in enumerate(retry):
         thing = valorant.update_database(player.puuid)
 
         if not thing[0]:
             error_count += 1
 
             if printer:
-                print(f'{thing[1]} at {player.ign}')
+                print(f'{i+1}/{total}: {thing[1]} at {player.ign}')
 
         else:
-            error_count -= 1
             update_count += int(thing[1])
 
             if int(thing[1]) > 0:
                 updatedList.append((player.ign, thing[1]))
                 
             if graph:
-                graphs.make_graph(puuid=player.puuid, ign=player.ign, update=False)
+                graphs.graph(puuid=player.puuid, ign=player.ign, update=False)
         
-        if printer:
-            print(f'completed retry {i+1}/{total}')
+            if printer:
+                print(f'{i+1}/{total}: success on 2nd attempt')
 
     with open('ztemp.txt','w') as f:
         f.write(str(updatedList))
@@ -94,4 +85,4 @@ def update_all(graph, printer=True,start=0):
     return printerz
 
 if __name__ == "__main__":
-    update_all(False)   
+    update_all(True)   
--- ritsu.py
+++ ritsu.py
@@ -1,63 +1,36 @@
 import discord
-from discord.ext import commands
 import configparser
-import graphs
-import valorant
-import mmr_history_updater
-import requests
-import json
-import malsearch
-from discord_slash import SlashCommand
-from discord_slash.utils.manage_commands import create_option, create_choice
-import random
-import playerclass
-from discord import app_commands
+from discord.ext import commands
+import typing
 
-def get_config():
-    c = configparser.ConfigParser()
-    c.read('config.ini')
-    return c['discord']['ritsu']
+def get_token():
+	c = configparser.ConfigParser()
+	c.read('config.ini')
+	return c['discord']['ritsu']
 
-token = get_config()
+token = get_token()
+guild_id = 731539222141468673
 intents = discord.Intents.default()
-intents.members = True
-help_command = commands.DefaultHelpCommand(no_category = 'Commands')
-client = commands.Bot(command_prefix='$',help_command = help_command, intents=intents)
-slash = SlashCommand(client, sync_commands=True)
-guild_ids = [509314650265878530, 731539222141468673]
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print("it started working")
-    try:
-        synced = await client.tree.sync()
-        print(f'Synced {len(synced)} commands')
-    except Exception as e:
-        print(e)
+    print(f'We have logged in as {bot.user}')
 
-@client.tree.command(name="hello")
-async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(f"yo {interaction.user.mention}", ephemeral=True)
+@bot.command()
+async def pong(ctx, arg, arg2: typing.Optional[str] = 'no'):
+	await ctx.send(arg + arg2)
+	print('yes')
 
-
-@client.tree.command(name="rps")
-@app_commands.choices(choices=[
-    app_commands.Choice(name="Rock", value="rock"),
-    app_commands.Choice(name="Paper", value="paper"),
-    app_commands.Choice(name="Scissors", value="scissors"),
-    ])
-async def rps(interaction: discord.Interaction, choices: app_commands.Choice[str]):
-    if (choices.value == 'rock'):
-        counter = 'paper'
-    elif (choices.value == 'paper'):
-        counter = 'scissors'
-    else:
-        counter = 'rock'
-
-    await interaction.response.send_message(f"yo {counter}")
-    
-@client.event
+@bot.event
 async def on_message(message):
-    await client.process_commands(message)
+    if message.author == bot.user:
+        return
 
-client.run(token)
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+    
+    await bot.process_commands(message)
+		
+bot.run(token)
