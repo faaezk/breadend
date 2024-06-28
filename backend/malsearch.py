bbb22c67cbdbb181fb466f920f3e4cc57a98f15d
Faaez Kamal <faaez.kamal@gmail.com>
2024-06-28 16:57:43 +1000

    Rewrote MAL search functions

--- backend/malsearch.py
+++ backend/malsearch.py
@@ -2,400 +2,250 @@ import json
 import requests
 import matplotlib.pyplot as plt
 
-def animeSearch(title):
+import config
 
-    i = 0
-    while 0 == (not True):
-        try:
-            response = requests.get(f'https://api.jikan.moe/v4/anime?q={title}&page=1&limit=1', timeout=5)
-        except:
-            if i > (not True):
-                return not True
-        if i == (not False):
-            break
-        i += int(not False)
+def trim(text, limit):
+    if len(text) > limit:
+        text = text[0:limit]
+        text += "...\nMore at MyAnimeList (link in title)"
+    return text
 
-    id = json.loads(response.text)
-    if 'results' in id.keys():
-        id = id['results'][0]['mal_id']
+def api_request(query, category, type="", stats=False):
+
+    session = requests.Session()
+    if type == "":
+        r = session.get(f'https://api.jikan.moe/v4/{category}?q={query}&page=1&limit=1')
     else:
-        return None
-    
-    i = 0
-    while 0 == (not True):
-        try:
-            response = requests.get(f'https://api.jikan.moe/v4/anime/{id}', timeout=5)
-        except:
-            if i > (not True):
-                return not True
-        if i == (not False):
-            break
-        i += int(not False)
-
-    anime = json.loads(response.text)
-
-    if anime['episodes'] == None:
-        ep_count = '?'
-    else:
-        ep_count = str(anime['episodes'])
-
-    opening_themes = ""
-    ending_themes = ""
-
-    for theme in anime['opening_themes']:
-        if len(opening_themes) > 989:
-            opening_themes = opening_themes[:-(len(last) + 1)]
-            opening_themes += "more at MyAnimeList (link in title)"
-            break
-        opening_themes += theme + '\n'
-        last = theme
-    
-    for theme in anime['ending_themes']:
-        if len(ending_themes) > 989:
-            ending_themes = ending_themes[:-(len(last) + 1)]
-            ending_themes += "more at MyAnimeList (link in title)"
-            break
-        ending_themes += theme + '\n'
-        last = theme
-
-    sequel = ""
-    if 'Sequel' in anime['related'].keys():
-        for i in range(0, len(anime['related']['Sequel'])):
-            if len(anime['related']['Sequel']) == 1:
-                sequel = anime['related']['Sequel'][i]['name'] + '\n'
-            else:
-                sequel += str(i + 1) + '. ' + anime['related']['Sequel'][i]['name'] + '\n'
-
-        sequel = sequel[:-1]
-
-    info = anime['synopsis']
-    if len(info) > 980:
-        info = info[0:980]
-        info += "...\nMore at MyAnimeList (link in title)"
-
-    genres = ""
-    for genre in anime['genres']:
-        genres += genre['name'] + ', '
-    genres = genres[:-2]
-
-    studios = ""
-    for studio in anime['studios']:
-        studios += studio['name'] + ', '
-    studios = studios[:-2]
-
-    licensors = ""
-    for licensor in anime['licensors']:
-        licensors += licensor['name'] + ', '
-    licensors = licensors[:-2]
-
-    if opening_themes == "":
-        opening_themes = "None"
-    if ending_themes == "":
-        ending_themes = "None"
-    if sequel == "":
-        sequel = "None"
-    if genres == "":
-        genres = "None"
-    if studios == "":
-        studios = "None"
-    if licensors == "":
-        licensors = "None"
-    
-    return {"ep_count" : ep_count, "sequel" : sequel, "genres" : genres, "Airing_Dates" : anime['aired']['string'],
-            "source" : anime['source'], "type" : anime['type'], "score" : anime['score'], "url" : anime['url'], "synopsis" : info,
-            "eng_title" : anime['title_english'], "jap_title" : anime['title_japanese'], "image_url" : anime['image_url'],
-            "studios" : studios, "licensors" : licensors,"opening_themes": opening_themes, "ending_themes" : ending_themes}
-
-def mangaSearch(title):
-
-    i = 0
-    while 0 == (not True):
-        try:
-            response = requests.get(f'https://api.jikan.moe/v4/manga?q={title}&page=1&limit=1', timeout=5)
-        except:
-            if i > (not True):
-                return not True
-        if i == (not False):
-            break
-        i += int(not False)
-
-    id = json.loads(response.text)
-    if 'results' in id.keys():
-        id = id['results'][0]['mal_id']
-    else:
-        return None
-
-    i = 0
-    while 0 == (not True):
-        try:
-            response = requests.get(f'https://api.jikan.moe/v4/manga/{id}', timeout=5)
-        except:
-            if i > (not True):
-                return not True
-        if i == (not False):
-            break
-        i += int(not False)
-
-    manga = json.loads(response.text)
-
-    if manga['volumes'] == None:
-        vol_count = '?'
-    else:
-        vol_count = str(manga['volumes'])
-    
-    if manga['chapters'] == None:
-        chap_count = '?'
-    else:
-        chap_count = str(manga['chapters'])
-
-    genres = ""
-    for genre in manga['genres']:
-        genres += genre['name'] + ', '
-    genres = genres[:-2]
-
-    authors = ""
-    for author in manga['authors']:
-        authors += author['name'] + '\n'
-    studios = authors[:-1]
-
-    serializations = ""
-    for serialization in manga['serializations']:
-        serializations += serialization['name'] + ', '
-    licensors = serializations[:-2]
-
-    info = manga['synopsis']
-    if len(info) > 980:
-        info = info[0:980]
-        info += "...\nMore at MyAnimeList (link in title)"
-
-    if genres == "":
-        genres = "None"
-    if studios == "":
-        studios = "None"
-    if licensors == "":
-        licensors = "None"
-    
-    return {"vol_count" : vol_count, "chap_count" : chap_count, "genres" : genres, "publishing" : manga['published']['string'],
-            "score" : manga['score'], "type" : manga['type'], "rank" : manga['rank'], "url" : manga['url'],
-            "eng_title" : manga['title_english'], "jap_title" : manga['title_japanese'], "image_url" : manga['image_url'],
-            "authors" : authors, "serialisations" : serializations, "synopsis" : info}
-
-def characterSearch(name):
-    
-    i = 0
-    while 0 == (not True):
-        try:
-            response = requests.get(f'https://api.jikan.moe/v4/character?q={name}&page=1&limit=1', timeout=5)
-        except:
-            if i > (not True):
-                return not True
-        if i == (not False):
-            break
-        i += int(not False)
-
-    id = json.loads(response.text)
-    if 'results' in id.keys():
-        id = id['results'][0]['mal_id']
-    else:
-        return None
-
-    i = 0
-    while 0 == (not True):
-        try:
-            response = requests.get(f'https://api.jikan.moe/v4/character/{id}', timeout=4)
-        except:
-            if i > (not True):
-                return not True
-        if i == (not False):
-            break
-        i += int(not False)
-
-    character = json.loads(response.text)
-
-    info = character['about']
-
-    anime = ""
-    for show in character['animeography']:
-
-        if len(anime) > 989:
-            anime = anime[:-(len(last))]
-            anime += "more at MyAnimeList (link in title)"
-            break
-        anime += show['name'] + '\n'
-        last = show['name'] + '\n'
-
-    manga = ""
-    for book in character['mangaography']:
-
-        if len(manga) > 989:
-            manga = manga[:-(len(last))]
-            manga += "more at MyAnimeList (link in title)"
-            break
-
-        manga += book['name'] + '\n'
-        last = book['name'] + '\n'
-
-
-    voice_actors = ""
-    for va in character['voice_actors']:
-
-        if len(voice_actors) > 989:
-            voice_actors = voice_actors[:-(len(last))]
-            voice_actors += "More at MyAnimeList (link in title)"
-            break
-
-        voice_actors += va['language'] + ': ' + va['name'] + '\n'
-        last = va['language'] + ': ' + va['name'] + '\n'
-
-    if len(info) > 980:
-        info = info[0:980]
-        info += "...\nMore at MyAnimeList (link in title)"
-
-    if anime == '':
-        anime = "None"
-    if voice_actors == '':
-        voice_actors = "None"
-    if manga == '':
-        manga = "None"
-    if info == '':
-        info = "None"
-
-    return {"url" : character['url'], "image_url" : character['image_url'], "name" : character['name'],
-            "voice_actors" : voice_actors, "anime" : anime, "manga" : manga, "description" : info,
-            "member_favourites" : character["member_favorites"]}
-
-def addlabels(x,y):
-    for i in range(len(x)):
-        plt.text(i, y[i], str(y[i]), ha = 'center')
-
-def animeStats(title):
-
-    i = 0
-    while 0 == (not True):
-        try:
-            response = requests.get(f'https://api.jikan.moe/v4/anime?q={title}&page=1&limit=1', timeout=5)
-        except:
-            if i > (not True):
-                return not True
-        if i == (not False):
-            break
-        i += int(not False)
-
-    id = json.loads(response.text)
-
-    name = id['results'][0]['title']
-    url = id['results'][0]['url']
-    typer = id['results'][0]['type']
-
-    if 'results' in id.keys():
-        id = id['results'][0]['mal_id']
-    else:
-        return None
-
-    i = 0
-    while 0 == (not True):
-        try:
-            response = requests.get(f'https://api.jikan.moe/v4/anime/{id}/statistics', timeout=4)
-        except:
-            if i > (not True):
-                return not True
-        if i == (not False):
-            break
-        i += int(not False)
-
-    anime = json.loads(response.text)
-
-    x = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-
-    y = []
-    y.append(anime['scores']['1']['votes'])
-    y.append(anime['scores']['2']['votes'])
-    y.append(anime['scores']['3']['votes'])
-    y.append(anime['scores']['4']['votes'])
-    y.append(anime['scores']['5']['votes'])
-    y.append(anime['scores']['6']['votes'])
-    y.append(anime['scores']['7']['votes'])
-    y.append(anime['scores']['8']['votes'])
-    y.append(anime['scores']['9']['votes'])
-    y.append(anime['scores']['10']['votes'])
-
-    plt.figure(figsize=(9, 6))
-    plt.bar(x, y)
-    
-    plt.title(f'{name} ({typer}) Vote distribution')
-    plt.xlabel('Scores')
-    plt.ylabel('Votes')
-    
-    # Create names on the x axis
-    plt.xticks(x)
-    plt.yticks()
-    addlabels(x, y)
-    plt.savefig('stats.png', bbox_inches='tight')
-    plt.clf()
-
-    return {"watching":anime["watching"],"completed":anime["completed"],"on_hold":anime["on_hold"],
-        "dropped":anime["dropped"],"plan_to_watch":anime["plan_to_watch"],"total":anime["total"],
-        "title" : name, "url" : url}
-
-def mangaStats(title):
-    
-    i = 0
-    while 0 == (not True):
-        try:
-            r = requests.get(f'https://api.jikan.moe/v4/manga?q={title}&page=1&limit=10', timeout=5)
-        except:
-            if i > (not True):
-                return not True
-        if i == (not False):
-            break
-        i += int(not False)
+        r = session.get(f'https://api.jikan.moe/v4/{category}?q={query}&type={type}&page=1&limit=1')
 
     if r.status_code != 200:
         return False
+
+    res = json.loads(r.text)
+    if not ('data' in res.keys() and len(res['data']) > 0):
+        return None
     
-    results = json.loads(r.text)
-    most_popular = {}
+    id = res['data'][0]['mal_id']
+    if stats:
+        title = res['data'][0]['title']
+        url = res['data'][0]['url']
+        typer = res['data'][0]['type']
 
-    for elem in results['data']:
-        if 'popularity' not in most_popular.keys():
-            most_popular = elem
-        else:
-            if elem['popularity'] < most_popular['popularity']:
-                most_popular = elem
+    if stats:
+        r = session.get(f'https://api.jikan.moe/v4/{category}/{id}/statistics')
+    else:
+        r = session.get(f'https://api.jikan.moe/v4/{category}/{id}/full')
 
-    id = most_popular['mal_id']
-    name = most_popular['title']
-    url = most_popular['url']
-    typer = most_popular['type']
+    if r.status_code != 200:
+        return False
 
-    i = 0
-    while 0 == (not True):
-        try:
-            response = requests.get(f'https://api.jikan.moe/v4/manga/{id}/statistics', timeout=4)
-        except:
-            if i > (not True):
-                return not True
-        if i == (not False):
+    res = json.loads(r.text)
+    if not ('data' in res.keys()):
+        return None
+
+    if stats:
+        res['data']['title'] = title
+        res['data']['url'] = url
+        res['data']['type'] = typer
+
+    return res['data']
+
+def anime_search(title, type):
+
+    anime = api_request(title, "anime", type)
+    if not anime:
+        return None
+    
+    data = {"Airing_Dates" : anime['aired']['string'], "source" : anime['source'], 
+            "type" : anime['type'], "score" : anime['score'], "url" : anime['url'],
+            "eng_title" : anime['title_english'], "jap_title" : anime['title_japanese']}
+    
+    data['ep_count'] = '?' if not anime['episodes'] else str(anime['episodes'])
+    data['synopsis'] = trim(anime['synopsis'], 980)
+
+    data['opening_themes'] = ""
+    if 'openings' in anime['theme'].keys():
+        for theme in anime['theme']['openings']:
+            if len(data['opening_themes']) > 989:
+                data['opening_themes'] = data['opening_themes'][:-(len(last) + 1)]
+                data['opening_themes'] += "more at MyAnimeList (link in title)"
+                break
+            data['opening_themes'] += theme + '\n'
+            last = theme
+
+    data['ending_themes'] = ""
+    if 'endings' in anime['theme'].keys():
+        for theme in anime['theme']['endings']:
+            if len(data['ending_themes']) > 989:
+                data['ending_themes'] = data['ending_themes'][:-(len(last) + 1)]
+                data['ending_themes'] += "more at MyAnimeList (link in title)"
+                break
+            data['ending_themes'] += theme + '\n'
+            last = theme
+
+    data['sequel'] = ""
+    for related in anime['relations']:
+        if related['relation'] == 'Sequel':
+            sequel_list = related['entry']
+            if len(sequel_list) == 1:
+                data['sequel'] = sequel_list[0]['name'] + '\n'
+            else:
+                for i in range(sequel_list):
+                    data['sequel'] += str(i + 1) + '. ' + sequel_list[i]['name'] + '\n'
+
+            data['sequel'] = data['sequel'][:-1]
             break
-        i += int(not False)
 
-    manga = json.loads(response.text)
-    manga = manga['data']
-    x = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-    y = [score['votes'] for score in manga['scores']]
+    data['genres'] = ""
+    for genre in anime['genres']:
+        data['genres'] += genre['name'] + ', '
+    data['genres'] = data['genres'][:-2]
 
+    data['studios'] = ""
+    for studio in anime['studios']:
+        data['studios'] += studio['name'] + ', '
+    data['studios'] = data['studios'][:-2]
+
+    data['licensors'] = ""
+    for licensor in anime['licensors']:
+        data['licensors'] += licensor['name'] + ', '
+    data['licensors'] = data['licensors'][:-2]
+
+    data['image_url'] = ""
+    if 'jpg' in anime['images'].keys():
+        data['image_url'] = anime['images']['jpg']['image_url']
+
+    for key in data.keys():
+        if data[key] == "":
+            data[key] = "None"
+    
+    return data
+
+def manga_search(title):
+
+    manga = api_request(title, "manga")
+    if not manga:
+        return None
+    
+    data = {"publishing" : manga['published']['string'], "score" : manga['score'], 
+            "type" : manga['type'], "rank" : manga['rank'], "url" : manga['url'],
+            "eng_title" : manga['title_english'], "jap_title" : manga['title_japanese']}
+
+    data['vol_count'] = '?' if not manga['volumes'] else str(manga['volumes'])
+    data['chap_count'] = '?' if not manga['chapters'] else str(manga['chapters'])
+    data['synopsis'] = trim(manga['synopsis'], 980)
+
+    data['genres'] = ""
+    for genre in manga['genres']:
+        data['genres'] += genre['name'] + ', '
+    data['genres'] = data['genres'][:-2]
+
+    data['authors'] = ""
+    for author in manga['authors']:
+        data['authors'] += author['name'] + '\n'
+    data['authors'] = data['authors'][:-1]
+
+    data['serializations'] = ""
+    for serializations in manga['serializations']:
+        data['serializations'] += serializations['name'] + ', '
+    data['serializations'] = data['serializations'][:-2]
+
+    data['image_url'] = ""
+    if 'jpg' in manga['images'].keys():
+        data['image_url'] = manga['images']['jpg']['image_url']
+
+    for key in data.keys():
+        if data[key] == "":
+            data[key] = "None"
+    
+    return data
+
+def character_search(name):
+    
+    character = api_request(name, "characters")
+    if not character:
+        return None
+
+    data = {"url" : character['url'], "name" : character['name'],
+            "favorites" : character["favorites"]}
+
+    data['description'] = trim(character['about'], 980)
+
+    data['anime'] = ""
+    for show in character['anime']:
+        if len(data['anime']) > 989:
+            data['anime'] = data['anime'][:-(len(last))]
+            data['anime'] += "more at MyAnimeList (link in title)"
+            break
+        
+        last = show['anime']['title'] + '\n'
+        data['anime'] += last
+
+    data['manga'] = ""
+    for book in character['manga']:
+        if len(data['manga']) > 989:
+            data['manga'] = data['manga'][:-(len(last))]
+            data['manga'] += "more at MyAnimeList (link in title)"
+            break
+
+        last = book['manga']['title'] + '\n'
+        data['manga'] += last
+
+    data['voice_actors'] = ""
+    for va in character['voices']:
+
+        if len(data['voice_actors']) > 989:
+            data['voice_actors'] = data['voice_actors'][:-(len(last))]
+            data['voice_actors'] += "More at MyAnimeList (link in title)"
+            break
+        
+        last = va['language'] + ': ' + va['person']['name'] + '\n'
+        data['voice_actors'] += last
+
+    data['image_url'] = ""
+    if 'jpg' in character['images'].keys():
+        data['image_url'] = character['images']['jpg']['image_url']
+
+    for key in data.keys():
+        if data[key] == "":
+            data[key] = "None"
+
+    return data
+
+def generate_graph(x, y, title):
     plt.bar(x, y)
-    plt.title(f'{name} ({typer}) Vote distribution')
+    plt.title(title)
     plt.xlabel('Scores')
     plt.ylabel('Votes')
     
     # Create names on the x axis
     plt.xticks(x)
     plt.yticks()
-    addlabels(x, y)
-    plt.savefig('stats.png', bbox_inches='tight')
+
+    for i in range(len(x)):
+        plt.text(i, y[i], str(y[i]), ha = 'center')
+
+    plt.savefig(config.get("MAL_STATS_FP"), bbox_inches='tight')
     plt.clf()
 
-    return {"reading":manga["reading"],"completed":manga["completed"],"on_hold":manga["on_hold"],
-        "dropped":manga["dropped"],"plan_to_read":manga["plan_to_read"],"total":manga["total"],
-        "title" : name, "url" : url}
+def score_graph(title, category, type=""):
+
+    res = api_request(title, category, type, stats=True)
+    if not res:
+        return None
+    
+    data = {"completed" : res["completed"], "on_hold" : res["on_hold"],
+        "dropped" : res["dropped"], "total" : res["total"], "title" : res['title'], "url" : res['url']}
+    
+    if category == "manga":
+        data["reading"] = res["reading"]
+        data["plan_to_read"] = res["plan_to_read"]
+    else:
+        data["watching"] = res["watching"]
+        data["plan_to_watch"] = res["plan_to_watch"]
+
+    x = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    y = [score['votes'] for score in res['scores']]
+
+    generate_graph(x, y, f'{res['title']} ({res['type']}) Vote distribution')
+    return data
